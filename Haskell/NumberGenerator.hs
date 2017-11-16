2d55f5f5029310858137afadb3cda0572f001479
Davide Fantini <davide.fantini@mail.polimi.it>
2017-11-16 08:51:17 +0100

    bug fix in numbergenerator

--- Haskell/NumberGenerator.hs
+++ Haskell/NumberGenerator.hs
@@ -4,11 +4,11 @@ import Data.Char
 
 function12 :: Int -> [Int]
 function12 n = f2 1 n
-
+-- 1 2 3 4 5 6 7 8 9 10 11 12 13
 f2 :: Int -> Int -> [Int]
 f2 x y 
     |x+4 <= y = [x]++[x+1] ++ f2 (x+4) y
-    |x+4 > y = if((x+4 - y) == 3 ) 
+    |x+4 > y = if((x+4 - y) == 4 ) 
         then [x]
         else [x] ++ [x+1] 
 
@@ -34,6 +34,10 @@ digs x
 -- ex. printfunction12 10 = ["1", "2", "5", "6", "9", "10"]
 printfunction12 :: Int -> String
 printfunction12 n = addpattern (map intToChar (map digs (function12 n)))
+
+printfunction34 :: Int -> String
+printfunction34 n = addpattern (map intToChar (map digs (function34 n)))
+
 
 -- adds the pattern for the correct visualization
 addpattern :: [String] -> String
